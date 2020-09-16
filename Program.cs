5f77007874c0128d8377ac6bb85cc69ae954ac46
Evge-s <egoeny@gmail.com>
2020-09-16 14:59:25 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CertTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tasks/Task1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CertTasks.Tasks
+{
+    class Task1
+    {
+    }
+}
