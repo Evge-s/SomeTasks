b8ba8973c4b5da0548924be1d51334d9818c2a11
Evge-s <egoeny@gmail.com>
2020-10-17 12:53:43 +0300

    add 23-25 tasks

---
+++ Tasks/Task23.cs
@@ -0,0 +1,52 @@
+﻿/*1.  Задано 2 посортованих масиви. Очікуваний результат: посортований масив, який складатиметься з перших двох. 
+	Обходити масиви можна лише раз. Не застосовувати вбудовані ф-ції сортування
+
+	Приклад вхідних даних:
+	[2,4,8,34] 
+	[2,5,6,8,12,42,54] 
+
+	Очікуванеий результат:
+	[2,2,4,5,6,8,8,12,34,42,54]
+*/
+using System.Collections.Generic;
+
+namespace CertTasks.Tasks
+{
+    class Task23
+    {
+        public static int[] AddSortArrays(int[] first, int[] second)
+        {
+            // свап вказівників, щоб не виходити за межі массиву,   
+            // якщо розмір першого буде більший за розмір другого
+            if (first.Length > second.Length)
+            {
+                var temp = second;
+                second = first;
+                first = temp;
+            }
+
+            List<int> res = new List<int>();
+
+            // ітератори
+            int i = 0;
+            int j = 0;
+
+            // логіка порівняння, та додавання значень до вихідного
+            // массиву, за один прохід.
+            while (res.Count != first.Length + second.Length)
+            {
+                if (i < first.Length && first[i] < second[j])
+                {
+                    res.Add(first[i]);
+                    i++;
+                }
+                else
+                {
+                    res.Add(second[j]);
+                    j++;
+                }
+            }
+            return res.ToArray();
+        }
+    }
+}
---
+++ Tasks/Task25.cs
@@ -0,0 +1,36 @@
+﻿/*Знайти всі паліндроми, які показує годинник протягом доби. Годинник показує час у 24-годинному форматі.
+    Паліндром - слово, число чи набір символів, які однаково читаються зліва направо та справа наліво.	
+	Приклад:
+	21:12
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CertTasks.Tasks
+{
+    class Task25
+    {
+        public static List<string> Palindroms()
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < 24; i++) // години
+            {
+                for (int j = 0; j < 59; j++) // хвилини
+                {
+                    string temp = string.Empty;
+                    if (i < 10) // додаю 0 якщо година меньше 10
+                        temp = "0";
+                    temp += i.ToString() + ":";
+                    if (j < 10) // додаю 0 якщо хвилина меньше 10
+                        temp += "0";
+                    temp += j.ToString();
+                    // перевірка на паліндром
+                    if (temp.SequenceEqual(temp.Reverse()))
+                        result.Add(temp);
+                }
+            }
+            return result;
+        }
+    }
+}
